43af3fecdcc82b88dc24357baf7e42be084c1cfc
Richard Loveless III <richardlovelessiii@gmail.com>
2019-12-17 22:44:22 -0500

    index.html able to read and display json list

--- js/wishList.js
+++ js/wishList.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
 });
 
 function getList(data) {
-    console.log(data);
+    $.each(data, (key, value) => {
+        let divID = key;
+        $('#wish_list').append(
+            $('<ul id="itemGroup' + divID + '" class="container">#' + divID + '</ul>')
+        )
+        $.each(value, (key, item) => {
+            $('#itemGroup' + divID).append(
+                $('<li><a href="' + item.url + '">' + item.name + '</a></li>')
+            )
+        })
+    })
 }
